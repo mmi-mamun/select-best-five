c00c8ce5da852cdcc173ce026064f79d94def96c
mmi-mamun <muzahidulislam735@gmail.com>
2022-08-23 20:36:45 +0600

    Final Submit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-const btns = document.querySelectorAll(".btn-pl");
+//For calling player selection button
+const btns = document.querySelectorAll(".btn-player");
 let counter = 1;
 
+//For access all player selection button
 for(let i=0;i<btns.length;i++){
     btns[i].addEventListener("click", function(){
         var playerCount = document.querySelectorAll(".disabled");
@@ -14,7 +16,9 @@ for(let i=0;i<btns.length;i++){
 }
 
 
+
 // event handler for player cost 
+
 function playerCost(){
     const perPlayerCost = parseInt(document.getElementById("per_player_cost").value);
     var playerCount = document.querySelectorAll(".player_name");
@@ -23,3 +27,14 @@ function playerCost(){
     playerExpenses.innerHTML = playerNumber*perPlayerCost;
 }
 
+
+//Total cost calculate function using event listener
+const buttonTotal = document.getElementById("btn_total");
+buttonTotal.addEventListener("click", function(){
+    let coachFee = parseInt(document.getElementById("coach_fee").value);
+    let managerFee = parseInt(document.getElementById("manager_fee").value);
+    let playerExpenses = parseInt(document.getElementById("player_expenses").textContent);
+
+    let total = document.getElementById("total");
+    total.innerText = playerExpenses + coachFee + managerFee;
+})
