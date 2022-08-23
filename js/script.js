ff67190c1cec002e68a8be59a055a31310dc4818
mmi-mamun <muzahidulislam735@gmail.com>
2022-08-23 19:55:44 +0600

    Player selection by javaScript done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-console.log("Connected");
-console.log('From Now, I am working');
+
+const btns = document.querySelectorAll(".btn-pl");
+let counter = 1;
+
+// common function 
+
+function players(i) {
+    var playerSelector = "player-"+i;
+    let player = document.getElementById(`${playerSelector}`).innerText;
+    let list = document.querySelector(".selected-player");
+    list.innerHTML += `<p class="player_name">${counter++}. ${player}</p>`;
+    btns[i].innerText = "Selected";
+    btns[i].classList.add("disabled");
+}
+
+for(let i=0;i<btns.length;i++){
+    btns[i].addEventListener("click", function(){
+        var playerCount = document.querySelectorAll(".disabled");
+        if(playerCount.length<=4){
+            players(i);
+        }
+        else{
+            alert("Maximum five players are allowed to select.");
+        }
+    } )
+}
